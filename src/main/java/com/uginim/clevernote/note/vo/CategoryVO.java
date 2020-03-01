99506788115d761d2807c3c0a0c4bb750c81f82e
uginim <kimugiugi@gmail.com>
2020-03-01 23:45:16 +0900

    feat: Add noteservice class

--- src/main/java/com/uginim/clevernote/note/vo/CategoryVO.java
+++ src/main/java/com/uginim/clevernote/note/vo/CategoryVO.java
@@ -5,9 +5,9 @@ import java.util.Date;
 import lombok.Data;
 @Data
 public class CategoryVO {
-	String title;
-	long categoryNum;
-	long ownerNum;
+	String title; // 제목
+	long categoryNum; // 카데고리 번호
+	long ownerNum; // 유저 번호
 	Date createdAt; // 생성 시간
 	Date updatedAt; // 업데이트 시간
 }
